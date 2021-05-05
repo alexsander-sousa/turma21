8822c38793e16468e2cbb72f11109a1cb3551eda
Alexsander Sousa <alexsander.ssousa@hotmail.com>
2021-05-05 14:38:48 -0300

    Exercícios de 05.05.21

--- java/AULAS/src/Lista2/L2Ex2.java
+++ java/AULAS/src/Lista2/L2Ex2.java
@@ -14,7 +14,7 @@ public class L2Ex2 {
 		int num1, num2, num3, pNum=0, sNum=0, tNum=0;
 		
 		//input
-		System.out.println("      Ordem crecente      ");
+		System.out.println("     Ordem crescente      ");
 		System.out.println("--------------------------");
 		System.out.print("Digite o primeiro número: ");
 		num1 = read.nextInt();
@@ -24,36 +24,42 @@ public class L2Ex2 {
 		num3 = read.nextInt();
 
 		//processing and output
-		if(num1<num2) {
-			if(num1<num3) {
-				pNum = num1;
+		if(num1<=num2 && num1<=num3) {
+			if(num2>=num3) {
+				pNum=num1;
+				sNum=num3;
+				tNum=num2;
 			}
-			if(num1>num3) {
-				sNum = num1;
-		}
-		if(num2<num1) {
-			if(num2>num3) {
-				tNum = num2;
-			}
-			else if(num2<num3) {
-				sNum = num2;
-			}
-			else {
-				pNum = num2;
+			if(num3>=num2) {
+				pNum=num1;
+				sNum=num2;
+				tNum=num3;
 			}
 		}
-		if(num3>num1) {
-			if(num3>num2) {
-				tNum = num3;
+		if(num2<=num1 && num2<=num3) {
+			if(num1>=num3) {
+				pNum=num2;
+				sNum=num3;
+				tNum=num1;
 			}
-			else if(num3<num2) {
-				sNum = num3;
+			if(num3>=num1) {
+				pNum=num2;
+				sNum=num1;
+				tNum=num3;
 			}
-			else if(num3<num1){
-				pNum = num3;
+		}
+		if(num3<num1 && num3<num2) {
+			if(num1>=num2) {
+				pNum=num3;
+				sNum=num2;
+				tNum=num1;
 			}
-		}		
+			if(num2>=num1) {
+				pNum=num3;
+				sNum=num1;
+				tNum=num2;
+			}
+		}
 		System.out.printf("\nNúmero digitados em ordem crecente: %d , %d , %d .",pNum,sNum,tNum);
 		}
-	}
 }
